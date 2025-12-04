41e538e7f5a1f70624f6c17201c467af8459527f
vixark <vixark@outlook.com>
2025-12-04 16:17:46 -0500

    Se ha añadido un aviso de derechos de autor en todos los archivos de código fuente para cumplir con la licencia AGPL v3.
    Se ha introducido un archivo `.editorconfig` con reglas de estilo
    de codificación, incluyendo encabezados obligatorios, tratamiento
    de advertencias de nullables como errores, y preferencias de
    formato y nomenclatura.
    
    Se han habilitado analizadores de .NET (`EnableNETAnalyzers`) en
    los archivos de proyecto y se ha establecido el nivel de análisis
    en `latest`.
    
    Se han simplificado validaciones de nulos con expresiones
    condicionales y eliminado directivas `#pragma warning` innecesarias.
    
    Se han realizado ajustes menores en el formato y organización del
    código, mejorando la claridad y consistencia.

--- Frugalia.Pruebas/FuncionesAuxiliares.cs
+++ Frugalia.Pruebas/FuncionesAuxiliares.cs
@@ -1,3 +1,27 @@
+// Copyright Notice:
+//
+// Frugalia® is a free LLM wrapper library that includes smart mechanisms to minimize AI API costs.
+// Copyright© 2025 Vixark (vixark@outlook.com).
+// For more information about Frugalia®, see https://frugalia.org.
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but without any warranty, without even the
+// implied warranty of merchantability or fitness for a particular purpose. See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not,
+// see https://www.gnu.org/licenses.
+//
+// This License does not grant permission to use the trade names, trademarks, service marks, or product names
+// of the Licensor, except as required for reasonable and customary use in describing the origin of the work
+// and reproducing the content of the notice file.
+//
+// When redistributing this file, preserve this notice, as required by the GNU Affero General Public License.
+//
+
 using System.Reflection;
 namespace Frugalia.Pruebas;
 
@@ -63,11 +87,9 @@ public class FuncionesAuxiliares {
             [typeof(string), typeof(bool), typeof(int?), typeof(int?), typeof(int?), typeof(int?), typeof(int?), typeof(int?)],
             modifiers: null);
 
-        if (ctor == null) {
-            throw new InvalidOperationException("No se encontró el constructor esperado de Tókenes.");
-        }
-
-        return (Tókenes)ctor.Invoke([
+        return ctor == null
+            ? throw new InvalidOperationException("No se encontró el constructor esperado de Tókenes.")
+            : (Tókenes)ctor.Invoke([
             modelo,
             lote,
             entradaTotal,
@@ -85,12 +107,9 @@ public class FuncionesAuxiliares {
 
         var propiedad = typeof(Tókenes).GetProperty(nombrePropiedad, BindingFlags.NonPublic | BindingFlags.Instance);
 
-        if (propiedad == null) {
-            throw new InvalidOperationException($"No se encontró la propiedad interna {nombrePropiedad} en Tókenes.");
-        }
-
-        return (int)(propiedad.GetValue(tokens) ?? 0);
-
+        return propiedad == null
+            ? throw new InvalidOperationException($"No se encontró la propiedad interna {nombrePropiedad} en Tókenes.")
+            : (int)(propiedad.GetValue(tokens) ?? 0);
     } // ObtenerPropiedadEntera>
 
 
@@ -98,11 +117,9 @@ public class FuncionesAuxiliares {
 
         var propiedad = typeof(Tókenes).GetProperty("Clave", BindingFlags.NonPublic | BindingFlags.Instance);
 
-        if (propiedad == null) {
-            throw new InvalidOperationException("No se encontró la clave de Tókenes.");
-        }
-
-        return (string)(propiedad.GetValue(tokens) ?? string.Empty);
+        return propiedad == null
+            ? throw new InvalidOperationException("No se encontró la clave de Tókenes.")
+            : (string)(propiedad.GetValue(tokens) ?? string.Empty);
 
     } // ObtenerClave>
 
--- Frugalia/Conversación.cs
+++ Frugalia/Conversación.cs
@@ -1,4 +1,28 @@
-﻿using OpenAI.Responses;
+﻿// Copyright Notice:
+//
+// Frugalia® is a free LLM wrapper library that includes smart mechanisms to minimize AI API costs.
+// Copyright© 2025 Vixark (vixark@outlook.com).
+// For more information about Frugalia®, see https://frugalia.org.
+//
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+// General Public License as published by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but without any warranty, without even the
+// implied warranty of merchantability or fitness for a particular purpose. See the GNU Affero General Public
+// License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License along with this program. If not,
+// see https://www.gnu.org/licenses.
+//
+// This License does not grant permission to use the trade names, trademarks, service marks, or product names
+// of the Licensor, except as required for reasonable and customary use in describing the origin of the work
+// and reproducing the content of the notice file.
+//
+// When redistributing this file, preserve this notice, as required by the GNU Affero General Public License.
+//
+
+using OpenAI.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +32,6 @@ using static Frugalia.Global;
 namespace Frugalia {
 
 
-    #pragma warning disable OPENAI001 // Este tipo se incluye solo con fines de evaluación y está sujeto a cambios o a que se elimine en próximas actualizaciones. Suprima este diagnóstico para continuar.
     public class Conversación {
 
 
@@ -102,7 +125,6 @@ namespace Frugalia {
 
 
     } // Conversación>
-    #pragma warning restore OPENAI001 // Este tipo se incluye solo con fines de evaluación y está sujeto a cambios o a que se elimine en próximas actualizaciones. Suprima este diagnóstico para continuar.
 
 
 } // Frugalia>
